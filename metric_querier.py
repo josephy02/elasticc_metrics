3744e4e262b669eabba75cb8c134f5d73e50ef1c
Rob Knop (Nersc) <raknop@lbl.gov>
2023-04-06 07:11:12 -0700

    Added some new materialized views to the database re: probability differences.

--- metric_querier.py
+++ metric_querier.py
@@ -86,8 +86,9 @@ class ELAsTiCCMetricsQuerier:
      happen rarely or never.)
 
    * Call the methods tbin_val(tbin) and probbin_val(pbin) to get the
-     values at the middle of the bins.  You can pass either a scalar or
-     a numpy array for tbin or pbin, and will get the same thing back.
+     values at the middle of the bins in the table returned by
+     get_probhist.  You can pass either a scalar or a numpy array for
+     tbin or pbin, and will get the same thing back.
 
      tbin will return -32.t for tbin=0, even though the bin is [-∞,
      -30), and will return 102.5 even though the bin is [100, ∞).
@@ -97,6 +98,67 @@ class ELAsTiCCMetricsQuerier:
      probability, so this should never happend) and 1 for pbin=20 (the
      bin is [1.0, ∞), but well-behaved classifiers will only return 1.0
      in this bin, so this estimate should be correct.)
+
+   * Call the method right_probdiffs_for_object( diaObjectID ) to get a
+     dataframe that has
+       classifierId
+       trueClassId   [ will be identical in every row ]
+       earlytimebin
+       earlytimet0
+       earlytimet1
+       latetimebin
+       latetimet0
+       latetimet1
+       probdiff      [ max probability in late time bin minus max probability in early time bin ]
+      
+     Note that not all bins will be present -- only bins with data will
+     be present.  probdiff difference is the probability assigned to the
+     true class by the classifier between the late and early times.
+
+     The lowest time bin is [-99999, -20) and the highest time bin is
+     [100, 99999).  That nines are for internal database usage;
+     interpret these as "less than -20 days" and "100 days or greater".
+
+  * Call the method right_profdiffs_hist() to get a dataframe that has:
+      Indexes:
+        classifierId
+        trueClassId
+        earlytimebin
+        latetimebin
+        probdiffbin
+      Columns:
+        earlytimet0
+        earlytimet1
+        latetimet0
+        latetimet1
+        binmeanprobdiff
+        count
+        frac
+
+    For a given classifier and trueClassId, this looks at all the
+    probabilities for objects given to that classId by that classifier.
+
+    It looks at the differences in the maximum probabilities in the two
+    time ranges.
+
+    It histograms those differences, so that count is the number of
+    objects that end up in the early time bin, late time bin, and
+    probability difference bin.
+
+    Things named "*bin" are a bin number.  (early|late)time(0|1) give
+    the [t0, t1) range of that bin; see previous section for caveat
+    about lowest and highest time bin.  binmeanprobdiff gives the middle
+    of the probability bin (subject to floating point roundoff, so round
+    it to two decimal places when printing).  Note that the lowest and
+    highest probability bins are [-1.025, 0.975 ) and [ 0.975, 1.025 ).
+    The outside half of these bins are not actually part of the
+    distribution, so you'll need to rescale the counts to get a
+    propability density.  (If just plotting counts, probably plot the
+    bar half as wide at the low and high bins.)
+
+    frac is count divided by the sum of count over probability bins.
+
+    That's all complicated and stuff.
 
     """
 
@@ -251,3 +313,42 @@ class ELAsTiCCMetricsQuerier:
         rval[ probbin==0 ] = -1
         rval[ probbin==self._probbin_num+1 ] = 1
         return rval[0] if numpy.isscalar( inprobbin ) else rval
+
+
+    def right_probdiffs_for_object( self, diaObjectId ):
+        self.logger.debug( f"Sending query to get probability differences for object {diaObjectId}" )
+        rows = self.run_query( 'SELECT v."classifierId", v."trueClassId", '
+                               '  v.earlytimebin, tbe.dtmin AS earlytimet0, tbe.dtmax AS earlytimet1,'
+                               '  v.latetimebin, tbl.dtmin AS latetimet0, tbl.dtmax AS latetimet1,'
+                               '  probdiff ',
+                               'FROM elasticc_view_maxprobdiff v '
+                               'INNER JOIN elasticc_maxprob_timebins tbe ON v.earlytimebin=tbe.timebin '
+                               'INNER JOIN elasticc_maxprob_timebins tbl ON v.latetimebin=tbl.timebin '
+                               'WHERE v."diaObjectId"=%(objid)s '
+                               'ORDER BY "classifierId", earlytimebin, latetimebin',
+                               { 'objid': diaObjectId } )
+        self.logger.debug( f"Query done, pandafying" )
+        return pandas.DataFrame( rows )
+        
+
+    def right_probdiffs_hist( self ):
+        self.logger.debug( "Sending query to get the probability differences histogram thingy" )
+        rows = self.run_query( 'SELECT v."classifierId", v."trueClassId", '
+                               '  v.earlytimebin, tbe.dtmin AS earlytimet0, tbe.dtmax AS earlytimet1,'
+                               '  v.latetimebin, tbl.dtmin AS latetimet0, tbl.dtmax AS latetimet1,'
+                               '  probdiffbin, binmeanprobdiff, count '
+                               'FROM elasticc_view_maxprobdiff_hist v '
+                               'INNER JOIN elasticc_maxprob_timebins tbe ON v.earlytimebin=tbe.timebin '
+                               'INNER JOIN elasticc_maxprob_timebins tbl ON v.latetimebin=tbl.timebin '
+                               'ORDER BY "trueClassId", "classifierId", earlytimebin, latetimebin, probdiffbin ' )
+        self.logger.debug( "Query done, pandafying" )
+        df = pandas.DataFrame( rows )
+        df['binmeanprobdiff'] = numpy.array( df['binmeanprobdiff'], dtype=numpy.float64 )
+        df.set_index( [ 'classifierId', 'trueClassId', 'earlytimebin', 'latetimebin', 'probdiffbin' ], inplace=True )
+        df['frac'] = ( df['count']
+                       / df.groupby( ['classifierId','trueClassId',
+                                      'earlytimebin','latetimebin'] )['count'].sum() )
+        return df
+
+    def right_probdiffs_hist_probbin_mean( self, probbin ):
+        return -1.025 + 0.05/2 + (probbin-1) * 0.05
